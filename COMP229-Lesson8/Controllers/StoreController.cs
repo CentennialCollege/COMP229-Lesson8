603d603ae09b0f1dec841fe87f4eb15f4198af64
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-02 21:31:25 -0400

    added Store Controller and related Views

---
+++ COMP229-Lesson8/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP229_Lesson8.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "This is my store index page!";
+        }
+
+
+        // GET: Store/Browse?genre=Rock
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre= " + genre);
+
+            return message;
+        }
+
+
+        // GET: Store/Details/5
+        public string Details(int id)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Details, ID= " + id);
+
+            return message;
+        }
+    }
+}
